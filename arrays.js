7e84914cb31b29dcbac2dd68a97acc027ce998ce
Timothy Wyld <timwyld@gmail.com>
2019-11-14 00:21:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function removeElementFromBeginningOfArray(array){
 
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
-  return array
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
