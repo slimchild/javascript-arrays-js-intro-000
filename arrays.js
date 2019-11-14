464c6f4a1fa65a61f3f43c2176185ffba7049606
Timothy Wyld <timwyld@gmail.com>
2019-11-14 00:38:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function destructivelyAddElementToBeginningOfArray(array, item){
 
 function addElementToEndOfArray(array, item){
   var newArray = array;
-  newArray.push('foo');
+  newArray.push("foo");
   return newArray;
 }
 
